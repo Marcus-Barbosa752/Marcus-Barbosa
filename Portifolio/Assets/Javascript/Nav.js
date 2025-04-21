4f12d3c31c47e09b74545f0d108555c37e02ccec
Marcus-Barbosa752 <marcusbarbosa175@gmail.com>
2025-04-21 17:52:54 -0300

    Update Nav.js

--- Portifolio/Assets/Javascript/Nav.js
+++ Portifolio/Assets/Javascript/Nav.js
@@ -3,9 +3,11 @@ const LiProjetos = document.getElementById('LiProjetos')
 const LiSobre = document.getElementById('LiSobre')
 const IndicatoSession = document.getElementById('IndicatoSession')
 
+const Projetos = document.getElementById("Projetos")
+
 let InitialIndicator = 32
 
-function setIndicatorEvents(element, position) {
+function setIndicatorEvents(element, position, section) {
     element.onmouseover = () => {
         IndicatoSession.style.left = `${position}px`
     }
@@ -15,9 +17,19 @@ function setIndicatorEvents(element, position) {
     element.onclick = () => {
         InitialIndicator = position
         IndicatoSession.style.left = `${InitialIndicator}px`
+
+        if (section == "Home") {
+            Projetos.style.height = '0px'
+            return
+        }
+
+        if (section == "Projetos") {
+            Projetos.style.height = 'calc(100vh + 98%)'
+            return
+        }
     }
 }
 
-setIndicatorEvents(LiHome, 32)
-setIndicatorEvents(LiProjetos, 130)
-setIndicatorEvents(LiSobre, 230)
+setIndicatorEvents(LiHome, 32, "Home")
+setIndicatorEvents(LiProjetos, 130, "Projetos")
+setIndicatorEvents(LiSobre, 230, "Sobre")
