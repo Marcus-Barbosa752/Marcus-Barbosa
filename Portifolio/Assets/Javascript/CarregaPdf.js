3b2b778831178624d23f26e41515f91cc55befce
Marcus-Barbosa752 <marcusbarbosa175@gmail.com>
2025-08-11 21:43:57 -0300

    Delete Portifolio directory

--- Portifolio/Assets/Javascript/CarregaPdf.js
+++
@@ -1,8 +0,0 @@
-const CreateIframePdf = (Link, Saida) => {
-    const Iframe = `<embed src="${Link}" type="application/pdf"></embed>`
-
-    Saida.innerHTML = ''
-    Saida.innerHTML = Iframe
-}
-
-export { CreateIframePdf }
